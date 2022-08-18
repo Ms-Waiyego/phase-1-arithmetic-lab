67493e25d44ef0b5075e0d54bcce7827900f4802
Brenda <waiyegobrenda99@gmail.com>
2022-08-18 18:55:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+ var num1=31;
+ var num2=2;
+ var multiply = num1*num2
+
+var Number = 5;
+var random = Number;
+
+var num3=22
+var num4=9
+var mod =num3%num4
+
+var set= (1,2,3,4,5,16,17,18,20)
+var max = set
